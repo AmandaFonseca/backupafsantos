8eb884086fa0f7de8e03a9e2429a4153c142c9c7
AmandaFonseca <amandafonseca207@gmail.com>
2021-09-06 22:41:32 -0300

    validator de cpf

--- afscorretora/app/static/js/helpers/validator.js
+++ afscorretora/app/static/js/helpers/validator.js
@@ -1,3 +1,51 @@
+function verificarCPF(c){
+    let string = c.split('');
+    let num = string.length;
+    var i;
+    s = c;
+    var c = s.substr(0,9);
+    var dv = s.substr(9,2);
+    var d1 = 0;
+    var v = false;
+
+    for (i = 0; i < 9; i++){
+        d1 += c.charAt(i)*(10-i);
+    }
+    if((num > 11)||(num <11)){
+        v = false;
+        return v;
+    }
+    if (d1 == 0){
+        //alert("CPF Inválido")
+        v = false;
+        return v;
+    }
+    d1 = 11 - (d1 % 11);
+    if (d1 > 9) d1 = 0;
+    if (dv.charAt(0) != d1){
+       // alert("CPF Inválido")
+        v = false;
+        return v;
+    }
+
+    d1 *= 2;
+    for (i = 0; i < 9; i++){
+        d1 += c.charAt(i)*(11-i);
+    }
+    d1 = 11 - (d1 % 11);
+    if (d1 > 9) d1 = 0;
+    if (dv.charAt(1) != d1){
+       // alert("CPF Inválido")
+        v = false;
+        return v;
+    }
+    if (!v) {
+        //alert(c + "nCPF Válido")
+        v = true;
+        return v;
+    }
+}
+
 (function(win,doc){
     'use strict';
 
@@ -13,12 +61,20 @@
 
 
 
-    let email = document.querySelector('#id_email');
-    console.log(email);
+    let cpf = document.querySelector('#id_cpf');
+    let cpf_label = document.querySelector('.cpf_valido');
 
-    email.addEventListener ('keypress', (event) => {
-       const keyName = email.value;
-       console.log(keyName)
+    cpf.addEventListener ('blur', (event) => {
+        var aux = cpf.value;
+        const strNum = aux.replace(/[^0-9]/g, '')
+        var result = verificarCPF(strNum);
+        if(result){
+            cpf_label.classList.remove("ative");
+            cpf_label.classList.add("inative");
+        }else{
+            cpf_label.classList.remove("inative");
+            cpf_label.classList.add("ative");
+        }
     });
 
 })(window,document);
